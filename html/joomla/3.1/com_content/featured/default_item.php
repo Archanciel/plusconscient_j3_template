b469dd2787c2c6fe48741dce60de10ada47d792b
Archanciel <jp.schnyder@gmail.com>
2014-01-21 22:31:00 +0100

    David title underline fix

--- html/joomla/3.1/com_content/featured/default_item.php
+++ html/joomla/3.1/com_content/featured/default_item.php
@@ -23,9 +23,9 @@ $info    = $this->item->params->get('info_block_position', 0);
 
 <?php if ($params->get('show_title')) : ?>
 	<h2 class="item-title">
-	<img src="./templates/rt_afterburner2/images/PostHeaderIcon.png" width="26px" />
+	<img src="../templates/rt_afterburner2/images/PostHeaderIcon.png" width="26" alt="Title Icon" style="padding-right:4px;" />
 	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>"> <?php echo $this->escape($this->item->title); ?></a>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>"><?php echo $this->escape($this->item->title); ?></a>
 	<?php else : ?>
 		<?php echo $this->escape($this->item->title); ?>
 	<?php endif; ?>
